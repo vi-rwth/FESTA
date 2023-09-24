19a4195b6f285f984b1772817d959772e632a06b
vi-rwth <133004724+vi-rwth@users.noreply.github.com>
2023-09-24 21:42:42 +0200

    Update assign_serial.py
    -added periodicity support
    -fixed bug with CCL algorithm
    -MDAnalysis now main printing module

--- assign_serial.py
+++ assign_serial.py
@@ -9,9 +9,9 @@ import tqdm
 import MDAnalysis as mda
 import matplotlib.pyplot as plt
 from matplotlib import ticker
+import operator as op
 import warnings
 warnings.filterwarnings('ignore')
-import operator as op
  
 try:
   topo = sys.argv[4]
@@ -40,15 +40,18 @@ def group_numbers(numbers, max_diff):
     while any(tmplist):  
         min_distance = max_diff
         found = False
-        tmplist.remove(seed_elem)
+        try:
+            tmplist.remove(seed_elem)
+        except ValueError:
+            pass
         for compare_elem in tmplist: 
-            if ((seed_elem[0]-compare_elem[0])**2 + (seed_elem[1]-compare_elem[1])**2)**0.5< min_distance: 
+            if ((seed_elem[0]-compare_elem[0])**2 + (seed_elem[1]-compare_elem[1])**2)**0.5 < min_distance: 
                 found = True
                 min_distance = ((seed_elem[0]-compare_elem[0])**2 + (seed_elem[1]-compare_elem[1])**2)**0.5
                 min_elem = compare_elem
         if found == True and any(subgroup):
             subgroup.append(seed_elem)
-            seed_elem = min_elem                 
+            seed_elem = min_elem                   
         else:
             if any(subgroup):  
                 separate_groups.append(subgroup)
@@ -57,7 +60,7 @@ def group_numbers(numbers, max_diff):
             min_distance = max_diff
             for group in separate_groups:
                 for elem in group:
-                    if ((seed_elem[0]-elem[0])**2 + (seed_elem[1]-elem[1])**2)**0.5< min_distance:
+                    if  ((seed_elem[0]-elem[0])**2 + (seed_elem[1]-elem[1])**2)**0.5 < min_distance:
                         group.append(seed_elem)
                         sec_run = True
                         break
@@ -65,9 +68,63 @@ def group_numbers(numbers, max_diff):
                     break
             if sec_run == False:
                 subgroup.append(seed_elem)
-            if any(tmplist):
+            elif any(tmplist):
                 seed_elem = tmplist[0]
     return separate_groups
+
+def sort_pdb_cp2k(i, ref_point):
+    min_coords, shift_vect, print_out = [], [0,0,0], []
+    count = it.count(0)
+    for k in range(head+(indx*(atom_count+3)), head+((indx+1)*(atom_count+3))):
+        if lines[k].startswith('ATOM'):
+            tmp_count = next(count)
+            print_lines = []
+            str_part_1 = lines[k].split('                 ')[0]
+            str_part_2 = lines[k].split('0.00  0.00')[1]
+            if not len(" ".join(lines[k].split()).split(' ')) < 9:
+                atom_coords =[float(" ".join(lines[k].split()).split(' ')[3]),float(" ".join(lines[k].split()).split(' ')[4]),float(" ".join(lines[k].split()).split(' ')[5])]
+            else:
+                if len(" ".join(lines[k].split()).split(' ')[3]) > 8:
+                    tmp_str = " ".join(lines[k].split()).split(' ')[3].split('-')
+                    if len(tmp_str) == 2:
+                        atom_coords = [float(tmp_str[0]),float(tmp_str[1])*(-1),float(" ".join(lines[k].split()).split(' ')[4])]
+                    elif len(tmp_str) == 4:
+                        atom_coords = [float(tmp_str[1])*(-1),float(tmp_str[2])*(-1),float(tmp_str[3])*(-1)]
+                    else:
+                        if len(tmp_str[0]) == 0:
+                            atom_coords = [float(tmp_str[1])*(-1),float(tmp_str[2])*(-1),float(" ".join(lines[k].split()).split(' ')[4])]
+                        else:
+                            atom_coords = [float(tmp_str[0]),float(tmp_str[1])*(-1),float(tmp_str[2])*(-1)]
+                elif len(" ".join(lines[k].split()).split(' ')[4]) > 8:
+                    tmp_str = " ".join(lines[k].split()).split(' ')[4].split('-')
+                    atom_coords = [float(" ".join(lines[k].split()).split(' ')[3]),float(tmp_str[0]),float(tmp_str[1])*(-1)]
+            if o == 0 and tmp_count == 0:
+                ref_point = atom_coords
+            if not o == 0 and tmp_count == 0:
+                shift_vect = (np.array(ref_point) - np.array(atom_coords))
+            min_coords.append((np.array(atom_coords) + np.array(shift_vect)).tolist())
+            print_lines = ''.join((str_part_1,' '*(25-len(str(round(min_coords[tmp_count][0],3)))),str(round(min_coords[tmp_count][0],3)),' '*(8-len(str(round(min_coords[tmp_count][1],3)))),str(round(min_coords[tmp_count][1],3)),' '*(8-len(str(round(min_coords[tmp_count][2],3)))),str(round(min_coords[tmp_count][2],3)),'  0.00  0.00',str_part_2))  
+            print_out.append(print_lines)
+        elif lines[k].startswith('REMARK'):
+            print_out.append(lines[k])
+        elif lines[k].startswith('CRYST1'):
+            print_out.append(lines[k])                
+    print_out.append('END\n')
+    tempfile.writelines(print_out)
+
+def sort_pdb_cp2k_prework():
+    f = open(traj, 'r+')
+    lines = f.readlines()
+    f.close()
+    atom_count, head = 0, 0
+    for line in lines:
+        if line.startswith('ATOM'):
+            atom_count += 1
+        elif line.startswith('END'):
+            break
+        elif line.startswith('AUTHOR') or line.startswith('TITLE'):
+            head += 1
+    return head, atom_count, lines
 
 print('working on directory: ' + T)
 outline, bins, coords = [], [], []
@@ -119,28 +176,49 @@ start1 = time.perf_counter()
 all_points, sorted_coords = [], []
 for i, elem in enumerate(a):
     all_points.append(shapely.geometry.Point(elem,b[i]))
-grouped_points = group_numbers(outline, 20*np.sqrt(8)*tolerance)
-#if edge:
-#    print('periodicity detected: boundaries will be considered periodic')
-#    pbc = []
-#    grouped_edges = group_numbers(edge, 20*np.sqrt(8)*tolerance)
-#    for i in range(len(grouped_edges)-1):
-#        if (abs(grouped_edges[i][0][0])-abs(grouped_edges[i+1][0][0]))**2 + (abs(grouped_edges[i][0][0])-abs(grouped_edges[i+1][1][0]))**2 < 0.01:
-#            pbc_group = []
-#            for j, groups in enumerate(grouped_points):
-#                if have_common_elem(grouped_edges[i],groups) or have_common_elem(grouped_edges[i+1],groups):
-#                    pbc_group.append(j)
-#                if len(pbc_group) == 2:
-#                    pbc.append(pbc_group)
-#                if len(pbc)*2 == len(grouped_edges):
-#                    break
-#    print(pbc)
-#quit()
+grouped_points = group_numbers(outline, 10*np.sqrt(8)*tolerance)
+
 polygons = []
 for groups in grouped_points:
     polygons.append(shapely.geometry.Polygon(groups))
     
-print(str(len(polygons)) + ' minima identified')
+if edge:
+    edge_points, pbc = [], []
+    grouped_edges = group_numbers(edge, 10*np.sqrt(8)*tolerance)
+    for i,group1 in enumerate(grouped_edges):
+        if sum(list(map(len, pbc))) >= len(grouped_edges):
+            break
+        expect_group, tmp_lst = [], []
+        for elem in group1:
+            tmp_pt = copy.deepcopy(elem)
+            if elem[0] == high_max_a:
+                tmp_pt[0] = low_max_a
+            elif elem[0] == low_max_a:
+                tmp_pt[0] = high_max_a
+            if elem[1] == high_max_b:
+                tmp_pt[1] = low_max_b
+            elif elem[1] == low_max_b:
+                tmp_pt[1] = high_max_b
+            expect_group.append(tmp_pt)
+        found_periodic = False
+        for j,group2 in enumerate(grouped_points):
+            if have_common_elem(group2, expect_group) or have_common_elem(group2, group1):
+                periodicity = True
+                found_periodic = True
+                tmp_lst.append(j)
+        if found_periodic == True:
+            if len(tmp_lst) == 1:
+                periodicity = False
+                break
+            elif i == 0:
+                print('periodicity detected: boundaries will be considered periodic')
+            pbc.append(tmp_lst)
+print(str(len(polygons)), end = ' ')
+if periodicity == True:
+    print('distinctive areas identified')
+else:
+    print('minima identified')
+    
 for polygon in tqdm.tqdm(polygons, desc='determining minima frames', leave=False):
     ab, coords = [], []
     for point in all_points:
@@ -157,28 +235,35 @@ print('processed ' + str(len(a)) + ' frames')
 print('found ' + str(tot_min_frames) + ' minima frames')
 print('time needed for minima frames identification step: ' + str(round(time.perf_counter() - start1,3)) + ' s')
 
+if periodicity == True:
+    sorted_coords_period, tot_pbc, desc = [], [], []
+    for elem in pbc:
+        desc.append(' + '.join(('CV1: ' + str(round(np.mean(grouped_points[j], axis=0)[0],4)) + ' CV2: ' + str(round(np.mean(grouped_points[j], axis=0)[1],4))) for j in elem))
+        help_list = []
+        for i in elem:
+            tot_pbc.append(i)
+            help_list += sorted_coords[i]
+        sorted_coords_period.append(help_list)
+    for i,elem in enumerate(sorted_coords):
+        if not i in tot_pbc:
+            desc.append('CV1: ' + str(round(np.mean(grouped_points[i], axis=0)[0],4)) + ' CV2: ' + str(round(np.mean(grouped_points[i], axis=0)[1],4)))
+            sorted_coords_period.append(elem)
+    sorted_coords = sorted_coords_period
+    print(str(len(sorted_coords)) + ' minima identified')
+
 if not os.path.isdir('minima'):
     os.mkdir('minima')
 
 start3 = time.perf_counter()
 
-if topo == None:
-    if traj.endswith('.pdb'):
-        f = open(traj, 'r+')
-        lines = f.readlines()
-        f.close()
-        atom_count, head = 0, 0
-        for line in lines:
-            if line.startswith('ATOM'):
-                atom_count += 1
-            elif line.startswith('END'):
-                break
-            elif line.startswith('AUTHOR') or line.startswith('TITLE'):
-                head += 1
-    else:
+try:
+    if topo == None:
         u = mda.Universe(traj, in_memory=True)
-else:
-    u = mda.Universe(topo, traj, in_memory=True)
+    else:
+        u = mda.Universe(topo, traj, in_memory=True)
+    ag =u.select_atoms('all')
+except IndexError:
+    head, atom_count, lines = sort_pdb_cp2k_prework()
     
 os.chdir('minima')
 with open('min_overview.txt', 'w') as overviewfile:
@@ -206,74 +291,33 @@ cb.update_ticks()
 plt.savefig('fes_visual.png',bbox_inches='tight')
 
 for i,elem in enumerate(sorted_coords):
+    indx_list = []
     with open('min_overview.txt', 'a') as overviewfile:
-        try:
-            overviewfile.writelines('min_' + str(i) + ': <CV1> = ' + str(round(np.mean(sorted_coords[i], axis=0)[0],4)) + ', <CV2> = ' + str(round(np.mean(sorted_coords[i], axis=0)[1],4)) + '\n')
-        except IndexError:
-            overviewfile.writelines('min_' + str(i) + ': No minima-frames found for this minimum\n')
-    if traj.endswith('.pdb'):
-        tempfile = open('min_' + str(i) + '.pdb', 'w')
-        ref_point = [0,0,0]
-    else:
-        indx_list = []
+        if periodicity == True:
+            overviewfile.writelines('min_' + str(i) + ': ' + desc[i] + '\n')
+        else:
+            overviewfile.writelines('min_' + str(i) + ': CV1: ' + str(round(np.mean(grouped_points[i], axis=0)[0],4)) + ' CV2: ' + str(round(np.mean(grouped_points[i], axis=0)[1],4)) + '\n')
     with tqdm.tqdm(total=len(elem), desc='min ' + str(i) + ': ' + str(len(elem)) + ' frames', leave=False) as progress_bar:
         for o in range(len(elem)):
             indx_a = index_search(sorted_coords[i][o][0],a)
             indx_b = index_search(sorted_coords[i][o][1],b)
             both = set(indx_a).intersection(indx_b)
             indx = both.pop()
-            if traj.endswith('.pdb'):
-                min_coords, shift_vect, print_out = [], [0,0,0], []
-                count = it.count(0)
-                for k in range(head+(indx*(atom_count+3)), head+((indx+1)*(atom_count+3))):
-                    if lines[k].startswith('ATOM'):
-                        tmp_count = next(count)
-                        print_lines = []
-                        str_part_1 = lines[k].split('                 ')[0]
-                        str_part_2 = lines[k].split('0.00  0.00')[1]
-                        if not len(" ".join(lines[k].split()).split(' ')) < 9:
-                            atom_coords =[float(" ".join(lines[k].split()).split(' ')[3]),float(" ".join(lines[k].split()).split(' ')[4]),float(" ".join(lines[k].split()).split(' ')[5])]
-                        else:
-                            if len(" ".join(lines[k].split()).split(' ')[3]) > 8:
-                                tmp_str = " ".join(lines[k].split()).split(' ')[3].split('-')
-                                if len(tmp_str) == 2:
-                                    atom_coords = [float(tmp_str[0]),float(tmp_str[1])*(-1),float(" ".join(lines[k].split()).split(' ')[4])]
-                                elif len(tmp_str) == 4:
-                                    atom_coords = [float(tmp_str[1])*(-1),float(tmp_str[2])*(-1),float(tmp_str[3])*(-1)]
-                                else:
-                                    if len(tmp_str[0]) == 0:
-                                        atom_coords = [float(tmp_str[1])*(-1),float(tmp_str[2])*(-1),float(" ".join(lines[k].split()).split(' ')[4])]
-                                    else:
-                                        atom_coords = [float(tmp_str[0]),float(tmp_str[1])*(-1),float(tmp_str[2])*(-1)]
-                            elif len(" ".join(lines[k].split()).split(' ')[4]) > 8:
-                                tmp_str = " ".join(lines[k].split()).split(' ')[4].split('-')
-                                atom_coords = [float(" ".join(lines[k].split()).split(' ')[3]),float(tmp_str[0]),float(tmp_str[1])*(-1)]
-                        if o == 0 and tmp_count == 0:
-                            ref_point = atom_coords
-                        if not o == 0 and tmp_count == 0:
-                            shift_vect = (np.array(ref_point) - np.array(atom_coords))
-                        min_coords.append((np.array(atom_coords) + np.array(shift_vect)).tolist())
-                        print_lines = ''.join((str_part_1,' '*(25-len(str(round(min_coords[tmp_count][0],3)))),str(round(min_coords[tmp_count][0],3)),' '*(8-len(str(round(min_coords[tmp_count][1],3)))),str(round(min_coords[tmp_count][1],3)),' '*(8-len(str(round(min_coords[tmp_count][2],3)))),str(round(min_coords[tmp_count][2],3)),'  0.00  0.00',str_part_2))  
-                        print_out.append(print_lines)
-                    elif lines[k].startswith('REMARK'):
-                        print_out.append(lines[k])
-                    elif lines[k].startswith('CRYST1'):
-                        print_out.append(lines[k])                
-                print_out.append('END\n')
-                tempfile.writelines(print_out)
-            else:
-                indx_list.append(indx)
+            indx_list.append(indx)
             progress_bar.update(1)
-    if traj.endswith('.pdb'):        
-        tempfile.close()
-    else:
-        ag =u.select_atoms('all')
-        try:
-            ag.write('min_' + str(i) + '.' + traj.split('.')[1], frames=u.trajectory[indx_list])
-        except IndexError:
+    try:
+        ag.write('min_' + str(i) + '.' + traj.split('.')[1], frames=u.trajectory[indx_list])
+    except (IndexError, NameError):
+        if traj.endswith('.pdb'):
+            tempfile = open('min_' + str(i) + '.pdb', 'w')
+            ref_point = [0,0,0]
+            for o in indx_list:
+                sort_pdb_cp2k(o, ref_point)
+            tempfile.close()
+        else:
             raise Exception('Multiple frames are not supported with this trajectory-format.')
-        except Exception:
-            print('MDAnalysis does not support writing of ' + traj.split('.')[1] + ' files, writing in xyz')
-            ag.write('min_' + str(i) + '.xyz', frames=u.trajectory[indx_list])
+    except (TypeError, ValueError):
+        print('MDAnalysis does not support writing in ' + traj.split('.')[1] + '-format, writing in xyz-format instead')
+        ag.write('min_' + str(i) + '.xyz', frames=u.trajectory[indx_list])
             
 print('time needed for postprocessing step: ' + str(round(time.perf_counter() - start3,3)) + ' s')
