3e87e81be541fec113c200ba7f01d9e98f1764a3
vi-rwth <133004724+vi-rwth@users.noreply.github.com>
2024-08-03 23:07:39 +0200

    Update FESTA.py
    - added unix optimization for PDBReader check
    - added f-strings replacing str.format
    - added input validity check for -png and -nopbc flags
    - updated calculation of default "-mindist" value to remain consistent for different grid densities
    - "-mindist" value no longer required to be in units of grid bins
    - updated "-mindist" default value to 2% of FES diagonal
    - fixed issue where PNG would be generated even when deactivated
    - fixed issue where PBC check would be run even when deactivated
    - fixed issue where Polygon generation would fail at low grid densities and low "-mindist" values

--- FESTA.py
+++ FESTA.py
@@ -13,33 +13,32 @@ import shutil
 import warnings
 import argparse
 import MDAnalysis as mda
+import subprocess
 
 warnings.filterwarnings('ignore', category=UserWarning)
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-traj', dest='traj', required=True, help='!REQUIRED! MD trajectory-file name in the MD-output-directory. Format is also used for output-files.', type=str)
-parser.add_argument('-md', dest='md_dir', default=os.getcwd(), help='MD-output-directory path. DEFAULT: Current directory path.')
+parser.add_argument('-md', dest='md_dir', default=os.getcwd(), help='MD-output-directory path. DEFAULT: Current directory path.', type=str)
 parser.add_argument('-thresh', dest='thresh', default=None, help='Specifies threshold for assigning. Input value has to correspond with values in FES-file. DEFAULT: Lowest 1/12 of the energy span.', type=float)
 parser.add_argument('-topo', dest='topo', default=None, help='MD topology-file name in the MD-output-directory, if trajectory-file does not specify topology. DEFAULT: None.', type=str)
 parser.add_argument('-fes', dest='fes', default='fes.dat', help='FES-file name in the MD-output-directory. DEFAULT: "fes.dat".', type=str)
-parser.add_argument('-colv', dest='colvar', default='COLVAR', help='COLVAR-file in the MD-output-directory. DEFAULT: "COLVAR".')
-parser.add_argument('-png', dest='fes_png', default=True, help='Specifies whether a PNG-visualization of the FES should be created. Expects True/False. DEFAULT: True.', type=bool)
-parser.add_argument('-nopbc', dest='nopbc', default=False, help='Suppresses the automatic periodicity search (triggered when minima touch the edges). Expects True/False. DEFAULT: False.', type=bool)
-parser.add_argument('-mindist', dest='mindist', default=10, help='Smallest allowed distance at which areas are considered separate minima (unit: bins of FES-histogram). Must be larger than 1. DEFAULT: 10.', type=float)
+parser.add_argument('-colv', dest='colvar', default='COLVAR', help='COLVAR-file in the MD-output-directory. DEFAULT: "COLVAR".', type=str)
+parser.add_argument('-png', dest='fes_png', default='True', help='Specifies whether a PNG-visualization of the FES should be created. Expects True/False. DEFAULT: True.', type=str)
+parser.add_argument('-nopbc', dest='nopbc', default='False', help='Suppresses the automatic periodicity search (triggered when minima touch the edges). Expects True/False. DEFAULT: False.', type=str)
+parser.add_argument('-mindist', dest='mindist', default=None, help='Smallest allowed distance at which areas are considered separate minima (in CV units). DEFAULT: 2% of FES diagonal.', type=float)
 parser.add_argument('-stride', dest='stride', default=1, help='Reads only every n-th frame of trajectory. DEFAULT: 1.', type=int)
 
 args = parser.parse_args()
     
 def init_polygon(Ga,Gb,Gtolerance,Gmp_sorted_coords,Ggrouped_points, barargs):
     tqdm.tqdm.set_lock(barargs)
-    global all_points
     global tolerance
-    global managed_list
+    global mp_sorted_coords
     global grouped_points
-    global a
-    global b
-    a,b, tolerance, managed_list, grouped_points = Ga,Gb, Gtolerance, Gmp_sorted_coords, Ggrouped_points
+    global a, b
+    a,b, tolerance, mp_sorted_coords, grouped_points = Ga,Gb, Gtolerance, Gmp_sorted_coords, Ggrouped_points
     
 def init_worker_pdb(Gsorted_indx, barargs):
     tqdm.tqdm.set_lock(barargs)
@@ -47,19 +46,22 @@ def init_worker_pdb(Gsorted_indx, barargs):
     sorted_indx = Gsorted_indx
     
 def det_min_frames(j):
-    all_points = [shapely.geometry.Point(a[i],b[i]) for i in range(len(a))]
-    polygon = shapely.geometry.Polygon(grouped_points[j])
-    convex_hull = shapely.geometry.Polygon(polygon.convex_hull)
-    if abs(1-(polygon.area/convex_hull.area)) > 0.4:
-        polygon = convex_hull
-        print('polygon ' + str(j) + ' did not initialize properly, using convex-hull')
+    all_points = [shapely.Point(a[i],b[i]) for i in range(len(a))]
+    polygon = shapely.Polygon(grouped_points[j])
+    convex_hull = polygon.convex_hull
+    try:
+        if abs(1-(polygon.area/convex_hull.area)) > 0.4:
+            polygon = convex_hull
+            print(f'polygon {j} did not initialize properly, using convex-hull')
+    except ZeroDivisionError:
+        pass
     indxes = []
-    with tqdm.tqdm(total=len(all_points), desc='min ' + str(j), position=tqdm.tqdm._get_free_pos()+j, leave=False) as pbar:
+    with tqdm.tqdm(total=len(all_points), desc=f'min {j}', position=tqdm.tqdm._get_free_pos()+j, leave=False) as pbar:
         for i,point in enumerate(all_points):
             if polygon.distance(point) <= tolerance:
                 indxes.append(i)
             pbar.update(1)
-        managed_list[j] = indxes
+        mp_sorted_coords[j] = indxes
 
 def have_common_elem(l1, l2):
     for elem in l2:
@@ -161,18 +163,18 @@ def printout(i):
     if args.stride > 1:
         sorted_indx[i] = [args.stride*q for q in sorted_indx[i]]
     try:
-        ag.write('minima/min_' + str(i) + '.' + args.traj.split('.')[-1], frames=u.trajectory[sorted_indx[i]])
+        ag.write(f'minima/min_{i}.{args.traj.split(".")[-1]}', frames=u.trajectory[sorted_indx[i]])
     except (TypeError, ValueError):
-        print('MDAnalysis does not support writing in ' + args.traj.split('.')[-1] + '-format, writing in xyz-format instead')
-        ag.write('minima/min_' + str(i) + '.xyz', frames=u.trajectory[sorted_indx[i]])
+        print(f'MDAnalysis does not support writing in {args.traj.split(".")[-1]}-format, writing in xyz-format instead')
+        ag.write(f'minima/min_{i}.xyz', frames=u.trajectory[sorted_indx[i]])
         
 def printout_pdb_cp2k(i):
     if args.stride > 1:
         sorted_indx[i] = [args.stride*q for q in sorted_indx[i]]
     linecount, printcount = 0, 0
-    with open('minima/min_' + str(i) + '.pdb', 'w') as minfile:
+    with open(f'minima/min_{i}.pdb', 'w') as minfile:
         with open(args.traj, 'r') as ftraj:
-            with tqdm.tqdm(total=len(sorted_indx[i]), desc='writing min ' + str(i), position=tqdm.tqdm._get_free_pos()+i, leave=False) as pbar:
+            with tqdm.tqdm(total=len(sorted_indx[i]), desc=f'writing min {i}', position=tqdm.tqdm._get_free_pos()+i, leave=False) as pbar:
                 for line in ftraj:
                     try:
                         if sorted_indx[i][printcount] == linecount:
@@ -186,8 +188,12 @@ def printout_pdb_cp2k(i):
                         linecount += 1
 
 def printout_pdb_cp2k_prework():
-    with open(args.traj, 'rb') as bfile:
-        line_count = sum(1 for _ in bfile)
+    try:
+        output = subprocess.Popen(['wc', '-l', args.traj], stdout=subprocess.PIPE).communicate()[0]
+        line_count = int(output.decode('utf-8').split(' ')[0])
+    except FileNotFoundError:
+        with open(args.traj, 'rb') as bfile:
+            line_count = sum(1 for _ in bfile)
     with open(args.traj, 'r') as tfile:
         atom_count, head = 0, 0
         for line in tfile:
@@ -200,8 +206,15 @@ def printout_pdb_cp2k_prework():
     return int((line_count-head)/(atom_count+3))
 
 if __name__ == '__main__':
+    start = time.perf_counter()
     title = '.: Free Energy Surface Trajectory Analysis - FESTA :.'
     termin = '.: terminated successfully :.'
+    
+    if not args.nopbc in ['True', 'False']:
+        raise Exception('Invalid input for -nopbc option: True/False expected')
+    if not args.fes_png in ['True', 'False']:
+        raise Exception('Invalid input for -png option: True/False expected')
+    
     try:
         terminal_size = os.get_terminal_size()[0]
     except OSError:
@@ -209,13 +222,11 @@ if __name__ == '__main__':
     print(' '*terminal_size)
     print(' '*int((terminal_size-len(title))/2) + title + ' '*int((terminal_size-len(title))/2))
     print(' '*terminal_size)
-    print('working on directory: ' + args.md_dir)
-    if args.mindist <= 1:
-        print('Invalid minimal separation distance specified. Using default.')
-        args.mindist = 10
+    print(f'working on directory: {args.md_dir}')
+
     outline = []
     count1, count2 = it.count(0), it.count(0)
-    thresh_val, tot_min_frames, dimX, dimY = 0, 0, 0, 1
+    tot_min_frames, dimX, dimY = 0, 0, 1
     os.chdir(args.md_dir)
     
     with open(args.colvar, 'r') as colvar_file:
@@ -245,11 +256,9 @@ if __name__ == '__main__':
             raise Exception('Non-finite value (NaN or inf) discovered in FES-file')
     
     if args.thresh == None:
-        thresh_val = max(ener) - abs(max(ener)-min(ener))*(1-1/12)
+        args.thresh = max(ener) - abs(max(ener)-min(ener))*(1-1/12)
         print('automatically determined', end =' ') 
-    else:
-        thresh_val = args.thresh
-    print('threshold value: ' + str(round(thresh_val,3)) + ' a.U.')
+    print(f'threshold value: {round(args.thresh,3)} a.U.')
         
     b_central = True
     if (b_fes[0] == b_fes[1]):
@@ -260,7 +269,7 @@ if __name__ == '__main__':
             if not elem == b_count:
                 dimY += 1
                 b_count = elem
-        high_max_a, high_max_b  = a_fes[dimX-1], b_fes[-1]
+        max_a, max_b  = a_fes[dimX-1], b_fes[-1]
         tolX = abs(a_fes[0]-a_fes[1])/2
         tolY = abs(b_fes[0]-b_fes[dimY])/2
     else:
@@ -272,23 +281,29 @@ if __name__ == '__main__':
             if not elem == a_count:
                 dimY += 1
                 a_count = elem
-        high_max_a, high_max_b  = a_fes[-1], b_fes[dimX-1]
+        max_a, max_b  = a_fes[-1], b_fes[dimX-1]
         tolX = abs(a_fes[0]-a_fes[dimY])/2
         tolY = abs(b_fes[0]-b_fes[1])/2
-    low_max_a, low_max_b = a_fes[0], b_fes[0]
+    min_a, min_b = a_fes[0], b_fes[0]
+    
+    if args.mindist == None:
+        args.mindist = np.sqrt((max_a-min_a)**2+(max_b-min_b)**2)*0.02
+    elif args.mindist < 2*np.sqrt(tolX**2+tolY**2):
+        raise Exception('Minimal separation distance must be larger than diagonal of a single bin.')
+    
     outline_show_a, outline_show_b, edge = [], [], []
     
     for i in tqdm.tqdm(range(len(ener)),desc='collecting outline', leave=False):
         try:
-            if ener[i]<thresh_val and (a_fes[i] == low_max_a or a_fes[i] == high_max_a or b_fes[i] == low_max_b or b_fes[i] == high_max_b or ener[i-1]>thresh_val or ener[i+1]>thresh_val or ener[i-dimY]>thresh_val or ener[i+dimY]>thresh_val or ener[i+1+dimY]>thresh_val or ener[i-1+dimY]>thresh_val or ener[i+1-dimY]>thresh_val or ener[i-1-dimY]>thresh_val):
-                if a_fes[i] == low_max_a or a_fes[i] == high_max_a or b_fes[i] == low_max_b or b_fes[i] == high_max_b:
+            if ener[i]<args.thresh and (a_fes[i] == min_a or a_fes[i] == max_a or b_fes[i] == min_b or b_fes[i] == max_b or ener[i-1]>args.thresh or ener[i+1]>args.thresh or ener[i-dimY]>args.thresh or ener[i+dimY]>args.thresh or ener[i+1+dimY]>args.thresh or ener[i-1+dimY]>args.thresh or ener[i+1-dimY]>args.thresh or ener[i-1-dimY]>args.thresh):
+                if a_fes[i] == min_a or a_fes[i] == max_a or b_fes[i] == min_b or b_fes[i] == max_b:
                     edge.append([a_fes[i],b_fes[i]])
                 outline.append([a_fes[i],b_fes[i]])
-                outline_show_a.append(abs((a_fes[i]-low_max_a)/((high_max_a-low_max_a)/dimX)))
-                outline_show_b.append(dimY-abs((b_fes[i]-low_max_b)/((high_max_b-low_max_b)/dimY)))
+                outline_show_a.append(abs((a_fes[i]-min_a)/((max_a-min_a)/dimX)))
+                outline_show_b.append(dimY-abs((b_fes[i]-min_b)/((max_b-min_b)/dimY)))
         except IndexError:
             pass
-    
+
     a, b = np.loadtxt(args.colvar, unpack=True, usecols=(pos_cvs_col[0], pos_cvs_col[1]))
     if args.stride > 1:
         a, b = a[0::args.stride], b[0::args.stride]
@@ -305,27 +320,27 @@ if __name__ == '__main__':
             u = mda.Universe(args.topo, args.traj)
         ag = u.select_atoms('all')
         if not int((len(u.trajectory)-1)/args.stride+1) == len(a):
-            raise Exception('COLVAR-file and trajectory-file must have similar step length, here: ' + str(len(a)) + ' vs ' + str(int((len(u.trajectory)-1)/args.stride+1)))
+            raise Exception(f'COLVAR-file and trajectory-file must have similar step length, here: {len(a)} vs {int((len(u.trajectory)-1)/args.stride+1)}')
     except IndexError:
         if args.traj.endswith('.pdb'):
             frame_count = printout_pdb_cp2k_prework()
             cp2k_pdb = True
             if not int((frame_count-1)/args.stride+1) == len(a):
-                raise Exception('COLVAR-file and trajectory-file must have similar step length, here: ' + str(len(a)) + ' vs ' + str(int((frame_count-1)/args.stride+1)))
+                raise Exception(f'COLVAR-file and trajectory-file must have similar step length, here: {len(a)} vs {int((frame_count-1)/args.stride+1)}')
         else:
             raise Exception('MDAnalysis does not support this topology- or trajectory-file')
     except FileNotFoundError:
         raise
     print('done')
     
-    start0 = time.perf_counter() 
-    grouped_points = ex3(outline, args.mindist*2*np.sqrt(tolX**2+tolY**2))
+    start0 = time.perf_counter()
+    grouped_points = ex3(outline, args.mindist)
     grouped_points = [groups for groups in grouped_points if len(groups)>3]
-    print('time needed for CCL step: ' + str(round(time.perf_counter() - start0,3)) + ' s')
+    print(f'time needed for CCL step: {round(time.perf_counter() - start0,3)} s')
 
     start1 = time.perf_counter()
     periodicity = False
-    if edge and args.nopbc == False:
+    if edge and args.nopbc == 'False':
         edge_points, pbc = [], []
         grouped_edges = ex3(edge, 10*2*np.sqrt(tolX**2+tolY**2))
         for i in range(len(grouped_edges)):
@@ -334,14 +349,14 @@ if __name__ == '__main__':
             expect_group, tmp_lst = [], []
             for elem in grouped_edges[i]:
                 tmp_pt = copy.deepcopy(elem)
-                if elem[0] == high_max_a:
-                    tmp_pt[0] = low_max_a
-                elif elem[0] == low_max_a:
-                    tmp_pt[0] = high_max_a
-                if elem[1] == high_max_b:
-                    tmp_pt[1] = low_max_b
-                elif elem[1] == low_max_b:
-                    tmp_pt[1] = high_max_b
+                if elem[0] == max_a:
+                    tmp_pt[0] = min_a
+                elif elem[0] == min_a:
+                    tmp_pt[0] = max_a
+                if elem[1] == max_b:
+                    tmp_pt[1] = min_b
+                elif elem[1] == min_b:
+                    tmp_pt[1] = max_b
                 expect_group.append(tmp_pt)
             found_periodic = False
             for j,group2 in enumerate(grouped_points):
@@ -375,15 +390,15 @@ if __name__ == '__main__':
     sorted_indx = list(mp_sorted_coords)
     for lists in sorted_indx:
         tot_min_frames += len(lists)
-    print('processed ' + str(len(a)) + ' frames')
-    print('found ' + str(tot_min_frames) + ' minima frames')
-    print('time needed for minima frames identification step: ' + str(round(time.perf_counter() - start1,3)) + ' s')
+    print(f'processed {len(a)} frames')
+    print(f'found {tot_min_frames} minima frames')
+    print(f'time needed for minima frames identification step: {round(time.perf_counter() - start1,3)} s')
     
     desc = []
     if periodicity == True:
         sorted_coords_period, tot_pbc  = [], []
         for elem in pbc:
-            desc.append(' + '.join((fes_var[pos_cvs_fes[0]+2] + ': ' + str(round(np.mean(grouped_points[j], axis=0)[0],4)) + ' '+fes_var[pos_cvs_fes[1]+2]+': ' + str(round(np.mean(grouped_points[j], axis=0)[1],4))) for j in elem))
+            desc.append(' + '.join((f'{fes_var[pos_cvs_fes[0]+2]}: {round(np.mean(grouped_points[j], axis=0)[0],4)} {fes_var[pos_cvs_fes[1]+2]}: {round(np.mean(grouped_points[j], axis=0)[1],4)}') for j in elem))
             help_list = []
             for i in elem:
                 tot_pbc.append(i)
@@ -393,7 +408,7 @@ if __name__ == '__main__':
             sorted_coords_period.append(help_list)
         for i,elem in enumerate(sorted_indx):
             if not i in tot_pbc:
-                desc.append(fes_var[pos_cvs_fes[0]+2]+': ' + str(round(np.mean(grouped_points[i], axis=0)[0],4)) + ' '+fes_var[pos_cvs_fes[1]+2]+': ' + str(round(np.mean(grouped_points[i], axis=0)[1],4)))
+                desc.append(f'{fes_var[pos_cvs_fes[0]+2]}: {round(np.mean(grouped_points[i], axis=0)[0],4)} {fes_var[pos_cvs_fes[1]+2]}: {round(np.mean(grouped_points[i], axis=0)[1],4)}')
                 sorted_coords_period.append(elem)
         sorted_indx = sorted_coords_period
         print(str(len(sorted_indx)), end=' ')
@@ -407,7 +422,7 @@ if __name__ == '__main__':
         shutil.rmtree('minima')
         os.mkdir('minima')
 
-    if args.fes_png == True:
+    if args.fes_png == 'True':
         bins = np.empty((dimY,dimX))
         if b_central == True:
             for i in range(dimY):
@@ -420,12 +435,12 @@ if __name__ == '__main__':
     
         plt.figure(figsize=(8,6), dpi=300)
         plt.imshow(bins, interpolation='gaussian', cmap='nipy_spectral')
-        plt.xticks(np.linspace(-0.5,dimX-0.5,5),np.round(np.linspace(low_max_a,high_max_a, num=5),3))
-        plt.yticks(np.linspace(-0.5,dimY-0.5,5),np.round(np.linspace(high_max_b,low_max_b, num=5),3))
-        plt.xlabel(fes_var[pos_cvs_fes[0]+2] + ' [a.U.]')
-        plt.ylabel(fes_var[pos_cvs_fes[1]+2] + ' [a.U.]')
+        plt.xticks(np.linspace(-0.5,dimX-0.5,5),np.round(np.linspace(min_a,max_a, num=5),3))
+        plt.yticks(np.linspace(-0.5,dimY-0.5,5),np.round(np.linspace(max_b,min_b, num=5),3))
+        plt.xlabel(f'{fes_var[pos_cvs_fes[0]+2]} [a.U.]')
+        plt.ylabel(f'{fes_var[pos_cvs_fes[1]+2]} [a.U.]')
         plt.axis('tight')
-        plt.title('threshold: ' + str(round(thresh_val,3)) + ' a.U.')
+        plt.title(f'threshold: {round(args.thresh,3)} a.U.')
         plt.plot(outline_show_a, outline_show_b, '.', color='white', markersize=2)
         cb = plt.colorbar(label='free energy [a.U.]', format="{x:.0f}")
         tick_locator = ticker.MaxNLocator(nbins=8)
@@ -437,9 +452,9 @@ if __name__ == '__main__':
     with open('minima/min_overview.txt', 'w') as overviewfile:
         for i in range(len(sorted_indx)):
             if desc:
-                overviewfile.writelines('min_' + str(i) + ': ' + desc[i] + '\n')
+                overviewfile.writelines(f'min_{i} : {desc[i]} \n')
             else:
-                overviewfile.writelines('min_' + str(i) + ': '+ fes_var[pos_cvs_fes[0]+2] +': ' + str(round(np.mean(grouped_points[i], axis=0)[0],4)) + ' '+fes_var[pos_cvs_fes[1]+2]+': ' + str(round(np.mean(grouped_points[i], axis=0)[1],4)) + '\n')
+                overviewfile.writelines(f'min_{i} : {fes_var[pos_cvs_fes[0]+2]}: {round(np.mean(grouped_points[i], axis=0)[0],4)} {fes_var[pos_cvs_fes[1]+2]}: {round(np.mean(grouped_points[i], axis=0)[1],4)}\n')
     if cp2k_pdb == False:
         for i in tqdm.tqdm(range(len(sorted_indx)), desc='printing to file', leave=False):
             printout(i)
@@ -447,6 +462,6 @@ if __name__ == '__main__':
         with mp.Pool(processes = usable_cpu, initializer=init_worker_pdb, initargs=(sorted_indx,mp.RLock(),)) as pool:
             pool.map(printout_pdb_cp2k, range(len(sorted_indx)))
 
-    print('time needed for postprocessing step: ' + str(round(time.perf_counter() - start3,3)) + ' s')
+    print(f'time needed for postprocessing step: {round(time.perf_counter() - start3,3)} s')
     print(' '*terminal_size)
     print(' '*int((terminal_size-len(termin))/2) + termin + ' '*int((terminal_size-len(termin))/2))
